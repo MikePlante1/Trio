d8541d857106212548b774d3fdd67edb65d05d2b
Mike Plante <mikeplante1@gmail.com>
2024-08-26 11:52:17 -0400

    hardcode watch increment to 0.5 instead of bolusIncrement

--- FreeAPSWatch WatchKit Extension/Views/BolusView.swift
+++ FreeAPSWatch WatchKit Extension/Views/BolusView.swift
@@ -9,11 +9,12 @@ struct BolusView: View {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         formatter.minimum = 0
-        formatter.maximum = Double((state.maxBolus ?? 5) / (state.bolusIncrement ?? 0.1)) as NSNumber
-        formatter.maximumFractionDigits = (state.bolusIncrement ?? 0.1) > 0.05 ? 1 : 2
-        formatter.minimumFractionDigits = (state.bolusIncrement ?? 0.1) > 0.05 ? 1 : 2
+        formatter.maximum = Double((state.maxBolus ?? 5) / 0.5) as NSNumber
+        formatter.maximumFractionDigits = 0.5 > 0.05 ? 1 : 2
+        formatter.minimumFractionDigits = 0.5 > 0.05 ? 1 : 2
         formatter.allowsFloats = true
-        formatter.roundingIncrement = Double(state.bolusIncrement ?? 0.1) as NSNumber
+        formatter.roundingIncrement = Double(0.5) as NSNumber
+        formatter.roundingMode = .down
         return formatter
     }
 
@@ -28,13 +29,13 @@ struct BolusView: View {
                     } label: { Image(systemName: "minus") }
                         .frame(width: geo.size.width / 4)
                     Spacer()
-                    Text(numberFormatter.string(from: (steps * Double(state.bolusIncrement ?? 0.1)) as NSNumber)! + " U")
+                    Text(numberFormatter.string(from: (steps * Double(0.5)) as NSNumber)! + " U")
                         .font(.headline)
                         .focusable(true)
                         .digitalCrownRotation(
                             $steps,
                             from: 0,
-                            through: Double((state.maxBolus ?? 5) / (state.bolusIncrement ?? 0.1)),
+                            through: Double((state.maxBolus ?? 5) / 0.5),
                             by: 1,
                             sensitivity: .medium,
                             isContinuous: false,
@@ -44,7 +45,7 @@ struct BolusView: View {
                     Button {
                         WKInterfaceDevice.current().play(.click)
                         let newValue = steps + 1
-                        steps = min(newValue, Double((state.maxBolus ?? 5) / (state.bolusIncrement ?? 0.1)))
+                        steps = min(newValue, Double((state.maxBolus ?? 5) / 0.5))
                     } label: { Image(systemName: "plus") }
                         .frame(width: geo.size.width / 4)
                 }
@@ -76,12 +77,12 @@ struct BolusView: View {
         }
         .navigationTitle("Enact Bolus")
         .onAppear {
-            steps = Double((state.bolusRecommended ?? 0) / (state.bolusIncrement ?? 0.1))
+            steps = Double((state.bolusRecommended ?? 0) / 0.5)
         }
     }
 
     private func enactBolus() {
-        let amount = steps * Double(state.bolusIncrement ?? 0.1)
+        let amount = steps * Double(0.5)
         state.addBolus(amount: amount)
     }
 }
